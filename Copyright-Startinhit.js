ea27ebeb607b4f89e2d36b9bd549b729855289e5
tuantinh <39516314+tuantinh@users.noreply.github.com>
2018-07-14 18:38:22 +0700

    Delete Copyright-Startinhit.js

--- Copyright-Startinhit.js
+++
@@ -1,9 +0,0 @@
-﻿<div style='clear: both;'/>     
-</div>
-<div id='copyright-lower'>
-<div class='copyright-lower'>
-<div class='copyright-lower-left'>Copyright 2018 &#169; <a href='https://www.startinhit.net' title='Star Tỉnh IT'>Star Tỉnh IT</a></div>
-<div class='copyright-lower-right'>Developed by Star Tỉnh IT</div>
-</div>
-</div>
-</div>
